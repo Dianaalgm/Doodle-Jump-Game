2eae057c81bb473ef37c7c99c9d7b321cf0d2a9d
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-04-26 10:49:18 -0400

    adding the doodler

--- app.js
+++ app.js
@@ -4,9 +4,32 @@ let boardWidth = 360;
 let boardHeight = 576;
 let context;
 
+//Doodler-----
+let doodlerWidth = 46 ;
+let doodlerHeight = 46;
+let doodlerX = boardWidth/2 - doodlerWidth/2 ;
+let doodlerY = boardHeight*7/8 - doodlerHeight ;
+let doodlerRightImg;
+let doodlerLeftImg;
+
+let doodler = {
+    img:null,
+    x: doodlerX,
+    y: doodlerY,
+    width: doodlerWidth,
+    height: doodlerHeight
+}
+
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight
     board.width = boardWidth
     context = board.getContext("2d"); //This is to be able to draw on the board
+
+    //This is to draw the doodler on the canvas
+    context.fillStyle = "green" ;
+    context.fillRect(doodler.x,doodler.y,doodler.width,doodler.height);
+
+
 }
